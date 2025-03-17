970e5c6e9f18f33e68d23f9760cd7a4ee5997403
rogerrabbit <cosa211@gmx.de>
2025-03-17 21:46:04 +0100

    Set up Android directory for HouseWeightCalculator

---
+++ android/app/src/main/java/com/houseweightcalculator/MainActivity.java
@@ -0,0 +1,13 @@
+// filepath: c:\Users\Dion\HouseWeightCalculator\android\app\src\main\java\com\houseweightcalculator\MainActivity.java
+package com.houseweightcalculator;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
